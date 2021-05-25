2db23d8cdd3f4c263f8b9cefa8dbfb8281d5b201
CourageSean <seancourage23@gmail.com>
2021-05-25 05:58:16 +0200

    seperate logs

---
+++ scripts/auth.js
@@ -0,0 +1,51 @@
+// listen for auth status changes
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log('user logged in:', user);
+    db.collection('guides')
+      .get()
+      .then((snapshot) => {
+        setupGuides(snapshot.docs);
+      });
+  } else {
+    setupGuides([]);
+  }
+});
+
+//signup
+const signupForm = document.querySelector('#signup-form');
+const logout = document.querySelector('#logout');
+const loginForm = document.querySelector('#login-form');
+
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // create user
+  auth.createUserWithEmailAndPassword(email, password).then((credential) => {
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+//logout
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {});
+});
+
+//login
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((credential) => {
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
